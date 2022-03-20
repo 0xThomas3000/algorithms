eeb094127a25aa446a0f7d1990bce21b25065aab
tuanit168 <tuanit168@gmail.com>
2022-03-20 08:31:44 +0700

    Addition at the beginning

--- Other/linked_list/linkedList.py
+++ Other/linked_list/linkedList.py
@@ -1,4 +1,18 @@
+from .node import Node
+
 class LinkedList:
   def __init__(self):
-      self.start = None
-      self.end = None
+    self.start = None
+    self.end = None
+
+  def insertBeg(self, data):
+    newNode = Node(data)
+    if (self.start == None):
+      self.start = newNode
+      self.end = newNode
+      return
+    self.start.prev = newNode
+    newNode.next = self.start
+    self.start = newNode
+
+  
