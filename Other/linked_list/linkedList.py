8bc7ef2f7585bcb1b68c7847a01aca1c88d2bfeb
tuanit168 <tuanit168@gmail.com>
2022-03-20 08:32:28 +0700

    Addition at the end

--- Other/linked_list/linkedList.py
+++ Other/linked_list/linkedList.py
@@ -15,4 +15,12 @@ class LinkedList:
     newNode.next = self.start
     self.start = newNode
 
-  
+  def insertEnd(self, x):
+    newNode = Node(x)
+    if (self.start == None):
+      self.start = newNode
+      self.end = newNode
+      return
+    self.end.next = newNode
+    newNode.prev = self.end
+    self.end = self.end.next
