4d85d53e829d8ffb10a64f8cf818a234c00a8f64
tuanit168 <tuanit168@gmail.com>
2022-03-27 07:45:24 +0700

    6.006 Merge sort

---
+++ MIT_6.006/merge_sort.py
@@ -0,0 +1,25 @@
+def merge_sort(A, a = 0, b = None): # T(b - a = n)
+  if b is None: b = len(A) # O(1)
+  if 1 < b - a: # O(1)
+    c = (a + b + 1) // 2 # O(1)
+    merge_sort(A, a, c) # T(n / 2)
+    merge_sort(A, c, b) # T(n / 2)
+    L, R = A[a:c], A[c:b] # O(n)
+    merge(L, R, A, len(L), len(R), a, b) # S(n)
+
+def merge(L, R, A, i, j, a, b): # S(b - a = n)
+  if a < b: # O(1)
+    if (j <= 0) or (i > 0 and L[i - 1] > R[j - 1]): # O(1)
+      A[b - 1] = L[i - 1] # O(1)
+      i = i - 1 # O(1)
+    else: # O(1)
+      A[b - 1] = R[j - 1] # O(1)
+      j = j - 1 # O(1)
+    merge(L, R, A, i, j, a, b - 1)
+
+def printArr(arr):
+  merge_sort(arr, 0, len(arr))
+  print(arr)
+
+printArr([7, 1, 5, 6, 2, 4, 9, 3])
+printArr([2, 4, 5, 7, 1, 2, 3, 6])
